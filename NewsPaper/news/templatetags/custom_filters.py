ec72e2eebafcf4fe2ebb65392dee76f493556c80
Diodant <driverf59@gmail.com>
2022-03-23 23:34:04 +0200

    new

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -8,6 +8,6 @@ BAD_WORDS = ['коза', 'редиска', 'редиске', 'осел']
 @register.filter(name='censor')
 def censor(value):
     for word in BAD_WORDS:
-        value = value.replace(word[0:], '*' * (len(word) - 1))
+        value = value.replace(word[1:], '*' * (len(word) - 1))
 
     return f'{value}'
