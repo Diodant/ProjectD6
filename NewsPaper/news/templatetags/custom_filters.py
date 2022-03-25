53f372954edb859c9886e3f51df8b2e93c44a460
Diodant <driverf59@gmail.com>
2022-03-25 18:52:05 +0200

    new

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -7,7 +7,10 @@ BAD_WORDS = ['коза', 'редиска', 'редиске', 'осел']
 
 @register.filter(name='censor')
 def censor(value):
+    if not isinstance(value, str):
+        raise ValueError('Нельзя цензурировать не строку')
+
     for word in BAD_WORDS:
         value = value.replace(word[1:], '*' * (len(word) - 1))
 
-    return f'{value}'
+    return value
